88dfd11c327c978a189720472195a60ecede12cf
Prometheus <96538997+MoanaSeeha@users.noreply.github.com>
2022-09-20 22:09:42 +0800

    making category page

--- src/pages/Category/index.js
+++ src/pages/Category/index.js
@@ -36,7 +36,7 @@ const Category = () => {
         <div className='absolute xl:text-7xl lg:text-5xl md:text-4xl text-xl top-1/2 font-extrabold left-1/2 -translate-x-1/2 -translate-y-1/2' style={{'textShadow': ' 0px 4px 12px rgba(0, 0, 0, 0.25)'}}>A passion and Art of Design</div>
       </div>
       <div className='bg-[#363f54] w-full p-10 relative -mt-[8%]'>
-        <div className=' flex flex-wrap justify-around items-center w-9/12 mx-auto -mt-[15%] lg:-mt-[10%]'>
+        <div className=' flex flex-wrap justify-around items-baseline w-9/12 mx-auto -mt-[15%] lg:-mt-[10%]'>
           {/* <div > */}
           {CateCardInfos.map((info, i) => {
             return <CategoryCard img={info.img} name={info.name} desc={info.desc} key={i}/>
